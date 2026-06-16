7f86829bb8d4287fc91989a2c60a779f86e8d665
Saideep <saideepsomineni99@gmail.com>
2026-06-16 15:47:18 +0530

    changes in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
 <header className="nav nav-scrolled relative flex items-center px-4 py-4">
   <div className="absolute left-1/2 -translate-x-1/2 text-2xl tracking-wider">
-    <Link to="/" onClick={() => window.location.reload()} >MovieVault</Link>
+    <Link to="/" >MovieVault</Link>
   </div>
 
   <Link to='/watchlist' className="ml-auto products-link">
